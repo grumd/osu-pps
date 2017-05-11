e3e318b5a9c20f6ecd43a170e4667c68b47295fd
grumd <grumd0@gmail.com>
2017-05-11 09:11:15 +0300

    need to fetch map info now

--- fetch-beatmaps-for-users.js
+++ fetch-beatmaps-for-users.js
@@ -3,20 +3,42 @@
 const axios = require('axios');
 const fs = require('fs');
 
+const truncateFloat = (x) => Math.floor(x * 100) / 100;
+
+const uniq = (a) => {
+  const seen = {};
+  return a.filter((item) => {
+    return seen.hasOwnProperty(item) ? false : (seen[item] = true);
+  });
+}
+
 const idsFileName = 'ids.json';
 const usersList = JSON.parse(fs.readFileSync(idsFileName));
-const uniqueUsersList = usersList.filter((user, index) => usersList.indexOf(user) === index);
+const uniqueUsersList = uniq(usersList);
 
 const apikey = JSON.parse(fs.readFileSync('config.json')).apikey;
 
 const url = (userId) => `https://osu.ppy.sh/api/get_user_best?k=${apikey}&u=${userId}&limit=100&type=id`;
 const getUniqueMapId = (score) => `${score.beatmap_id}_${score.enabled_mods}`;
-const getMagnitudeByIndex = (x) => Math.pow((Math.pow(x - 100, 2) / 10000), 20); // ((x-100)^2/10000)^20
+const getMagnitudeByIndex = (x) => truncateFloat(Math.pow((Math.pow(x - 100, 2) / 10000), 20)); // ((x-100)^2/10000)^20
 
 const maps = {};
 
+const calculatePp99 = (map) => {
+  const ppPerAccSum = map.pp.reduce((sum, pp, index) => {
+    return sum + pp / map.acc[index];
+  }, 0);
+  map.pp99 = truncateFloat(ppPerAccSum / map.pp.length * 99);
+  delete map.pp;
+  delete map.acc;
+};
+
 const recordData = (data) => {
-  data.forEach((score, index) => {
+  data.slice(0, 20).forEach((score, index) => {
+    Object.keys(score).forEach(key => {
+      const parsed = parseFloat(score[key]);
+      score[key] = isNaN(parsed) ? score[key] : parsed;
+    });
     const mapId = getUniqueMapId(score);
     const acc = 100 * (score.count300 + score.count100 / 3 + score.count50 / 6) / (score.countmiss + score.count50 + score.count100 + score.count300);
     if (!maps[mapId]) {
@@ -24,13 +46,19 @@ const recordData = (data) => {
         m: score.enabled_mods,
         b: score.beatmap_id,
         pp: [score.pp],
-        acc: [parseFloat(acc.toFixed(2))],
+        acc: [truncateFloat(acc)],
         x: getMagnitudeByIndex(index),
       };
     } else {
-      maps[mapId].x += getMagnitudeByIndex(index);
-      maps[mapId].pp.push(score.pp);
-      maps[mapId].acc.push(parseFloat(acc.toFixed(2)));
+      const currentMap = maps[mapId];
+      currentMap.x += getMagnitudeByIndex(index);
+      if (currentMap.pp99 === undefined) {
+        currentMap.pp.push(score.pp);
+        currentMap.acc.push(truncateFloat(acc));
+        if (currentMap.pp.length === 10) {
+          calculatePp99(currentMap);
+        }
+      }
     }
   });
 };
@@ -45,14 +73,27 @@ const fetchUser = (userId) => {
 
 uniqueUsersList.reduce((promise, user, index) => {
   return promise.then(() => {
-    console.log(`Recording data for user #${index}`)
+    console.log(`Recording data for user #${index}/${uniqueUsersList.length}`)
     return Promise.all([
       timeout(100),
       fetchUser(user),
-    ]);
+    ]).then(() => {
+      if (index % 100 === 0) {
+        console.log(`${Object.keys(maps).length} unique maps found! Saved.`);
+        fs.writeFileSync('result.json', JSON.stringify(maps));
+        const arrayMaps = Object.keys(maps).map(mapId => maps[mapId]);
+        fs.writeFileSync('result-array.json', JSON.stringify(arrayMaps));
+      }
+    });
   })
 }, Promise.resolve())
   .then(() => {
+    console.log(`${Object.keys(maps).length} unique maps found! Saved.`);
+    Object.keys(maps).forEach((mapId) => {
+      if (maps[mapId].pp99 === undefined) {
+        calculatePp99(maps[mapId]);
+      }
+    })
     fs.writeFileSync('result.json', JSON.stringify(maps));
     const arrayMaps = Object.keys(maps).map(mapId => maps[mapId]);
     fs.writeFileSync('result-array.json', JSON.stringify(arrayMaps));
--- fetch-users-list.js
+++ fetch-users-list.js
@@ -3,43 +3,79 @@
 const axios = require('axios');
 const fs = require('fs');
 
-const getUsersUrl = (page) => `https://osu.ppy.sh/p/pp/?m=0&s=3&o=1&f=&page=${page}`;
+const delay = ms => new Promise(r => setTimeout(r, ms));
+
+const countriesListUrl = 'https://osu.ppy.sh/p/countryranking';
+const getUsersUrl = (page, country) => `https://osu.ppy.sh/p/pp/?m=0&s=3&o=1&f=&page=${page}` + (country ? `&c=${country}` : '');
 const getIdList = (text) => text.match(/\/u\/[0-9]+/g).map(uLink => uLink.slice(3));
+
+const uniq = (a) => {
+  const seen = {};
+  return a.filter((item) => {
+    return seen.hasOwnProperty(item) ? false : (seen[item] = true);
+  });
+}
 
 const saveIds = (list, fileName = 'ids.json') => {
   if (fs.existsSync(fileName)) {
     const oldList = JSON.parse(fs.readFileSync(fileName));
-    fs.writeFileSync(fileName, JSON.stringify(oldList.concat(list)));
+    const newList = uniq(oldList.concat(list));
+    fs.writeFileSync(fileName, JSON.stringify(newList));
   } else {
     fs.writeFileSync(fileName, JSON.stringify(list));
   }
 }
 
-const startFetchingPages = (page = 1) => {
-  axios.get(getUsersUrl(page))
+const startFetchingPages = (page, country) => {
+  return axios.get(getUsersUrl(page, country))
     .catch((err) => {
       console.log('Error:', err.message);
-      setTimeout(() => {
-        startFetchingPages(page);
-      }, 5000);
+      console.log(err);
+      return delay(5000)
+        .then(() => startFetchingPages(page, country));
     })
-    .then(({ data }) => savePage(data, page));
+    .then(({ data }) => {
+      const has1000pp = data.match(/\d+,\d+pp/g);
+      if (!has1000pp) {
+        console.log('Users with more than 999pp were not found on this page');
+        return Promise.resolve();
+      }
+      return savePage(data, page, country);
+    });
 };
 
-const savePage = (data, page) => {
-  return new Promise((res) => {
-    saveIds(getIdList(data));
-    console.log(`Saved page #${page}`);
-    res();
-  })
-    .then(() => {
-      setTimeout(() => {
-        startFetchingPages(page + 1);
-      }, 300);
-    })
+const savePage = (data, page, country) => {
+  saveIds(getIdList(data));
+  console.log(`Saved page #${page} for ${country}`);
+  if (page >= 200) {
+    return Promise.resolve();
+  }
+  return delay(300)
+    .then(() => startFetchingPages(page + 1, country))
     .catch((err) => {
       console.log('Error saving list:', err.message);
     });
 }
 
-startFetchingPages();
+axios.get(countriesListUrl)
+  .then(({ data }) => {
+    const countriesList = data.match(/\?c=[A-Z]{2}/g).map(cLink => cLink.slice(3));
+
+    return countriesList
+      // .slice(8) // Pass first 7 countries
+      .reduce((prevProm, country) => {
+        return prevProm
+          .then(() => {
+            console.log(`Starting to fetch ${country}`);
+          })
+          .then(() => startFetchingPages(1, country))
+          .then(() => {
+            console.log(`Finished fetching ${country}. Will resume in 5 secs`);
+            return delay(5000);
+          })
+      }, Promise.resolve());
+  })
+  .then(() => {
+    console.log('Finished all countries!');
+  });
+
