327c0f6787f3d8d939b0e35d1a2a6c68eba33948
grumd <grumd0@gmail.com>
2017-05-12 18:55:09 +0300

    small fixes, moved css and js to separate file

---
+++ index.js
@@ -0,0 +1,165 @@
+var data = [];
+var showCount = 20;
+function secondsToFormatted(length) {
+  return `${Math.floor(length / 60)}:${('0' + (length % 60)).slice(-2)}`;
+}
+function formattedToSeconds(minutes, seconds) {
+  return minutes * 60 + seconds;
+}
+function modAllowed(selectValue, hasMod) {
+  return !['yes', 'no'].includes(selectValue) || (selectValue === 'yes' && hasMod) || (selectValue === 'no' && !hasMod);
+}
+function matchesMaxMin(value, min, max) {
+  return value >= min && value <= max;
+}
+function filterData(rawData) {
+  var searchText = $('#search').val() || '';
+  var pp = {
+    min: parseFloat($('#pp-min').val()) || 0,
+    max: parseFloat($('#pp-max').val()) || 9999,
+  };
+  var bpm = {
+    min: parseFloat($('#bpm-min').val()) || 0,
+    max: parseFloat($('#bpm-max').val()) || 9999,
+  };
+  var diff = {
+    min: parseFloat($('#diff-min').val().replace(',', '.')) || 0,
+    max: parseFloat($('#diff-max').val().replace(',', '.')) || 9999,
+  };
+  var length = {
+    min: formattedToSeconds(
+      parseFloat($('#len-min-min').val()) || 0,
+      parseFloat($('#len-sec-min').val()) || 0
+    ),
+    max: formattedToSeconds(
+      parseFloat($('#len-min-max').val()) || 99,
+      parseFloat($('#len-sec-max').val()) || 59
+    ),
+  };
+  var mods = {
+    dt: $('#dt').val(),
+    hd: $('#hd').val(),
+    hr: $('#hr').val(),
+    fl: $('#fl').val(),
+  };
+  return rawData.filter((map) => {
+    var mapMods = {
+      dt: (map.m & 64) == 64,
+      hd: (map.m & 8) == 8,
+      hr: (map.m & 16) == 16,
+      fl: (map.m & 1024) == 1024,
+      ht: (map.m & 256) == 256,
+    };
+    var mapLink = `http://osu.ppy.sh/b/${map.b}`;
+    var linkText = (`${map.art} - ${map.t} [${map.v}]`).toLowerCase();
+    var searchWords = searchText.toLowerCase().split(' ');
+    var searchMatches = searchWords.every(word =>
+      mapLink.includes(word) || linkText.includes(word)
+    );
+
+    return searchMatches
+      && matchesMaxMin(map.pp99, pp.min, pp.max)
+      && matchesMaxMin(map.bpm, bpm.min, bpm.max)
+      && matchesMaxMin(map.d, diff.min, diff.max)
+      && matchesMaxMin(map.l, length.min, length.max)
+      && modAllowed(mods.dt, mapMods.dt)
+      && modAllowed(mods.hd, mapMods.hd)
+      && modAllowed(mods.hr, mapMods.hr)
+      && modAllowed(mods.fl, mapMods.fl)
+      && (mods.dt !== 'ht' || mapMods.ht);
+  });
+}
+function updateTable() {
+  var tableBody = $('#table-body');
+  tableBody.empty();
+
+  var filteredData = filterData(data);
+
+  $.each(filteredData.slice(0, showCount), function(index, value) {
+    var okGlyph = '<span class="glyphicon glyphicon-ok"></span>';
+    var htGlyph = '<span class="glyphicon glyphicon-time"></span>';
+    var mods = {
+      dt: (value.m & 64) == 64,
+      hd: (value.m & 8) == 8,
+      hr: (value.m & 16) == 16,
+      fl: (value.m & 1024) == 1024,
+      ht: (value.m & 256) == 256,
+    };
+    var mapLink = `http://osu.ppy.sh/b/${value.b}`;
+    var linkText = value.art ? `${value.art} - ${value.t} [${value.v}]` : mapLink;
+
+    var bpm = mods.dt
+      ? `<span class="bpm fast">${value.bpm * 1.5}</span>(${value.bpm})`
+      : mods.ht
+      ? `<span class="bpm slow">${value.bpm * 0.75}</span>(${value.bpm})`
+      : value.bpm;
+
+    var row = $('<tr></tr>');
+    row.append($(`<td class="img-td"><img src="https://b.ppy.sh/thumb/${value.s}.jpg" /></td>`));
+    row.append($('<td></td>').append($('<a></a>').attr('href', mapLink).text(linkText)));
+    row.append($('<td class="text-center"></td>').text((+value.pp99).toFixed(0)));
+    row.append($('<td class="text-center"></td>').append(mods.ht ? $(htGlyph) : mods.dt ? $(okGlyph) : null));
+    row.append($('<td class="text-center"></td>').append(mods.hd ? $(okGlyph) : null));
+    row.append($('<td class="text-center"></td>').append(mods.hr ? $(okGlyph) : null));
+    row.append($('<td class="text-center"></td>').append(mods.fl ? $(okGlyph) : null));
+    row.append($('<td class="text-center"></td>').text(secondsToFormatted(value.l)));
+    row.append($('<td class="text-center"></td>').append(bpm));
+    row.append($('<td class="text-center"></td>').text(value.d));
+    row.append($('<td class="text-center"></td>').text((+value.x).toFixed(0)));
+    tableBody.append(row);
+  });
+  $('#show-more-div').empty();
+  if (filteredData.length > 0 && showCount < filteredData.length) {
+    $('#show-more-div').append($('<button onclick="showMore()" class="btn btn-success show-more-btn">show more</button>'));
+  }
+  if (filteredData.length === 0) {
+    $('#show-more-div').text('nothing to show, sorry');
+  }
+}
+function showMore() {
+  showCount += 20;
+  updateTable();
+}
+function applyFilters() {
+  showCount = 20;
+  updateTable();
+}
+$(document).ready(function(){
+  var loadingMessageUpdate = setTimeout(function() {
+    $('#loading').text('Loading... It can be slow for the first time, but after it loads the database will be saved in your browser\'s cache.');
+  }, 3000);
+  $.ajax({
+    url: "data-2017-05-12.json",
+    success: function(rawData) {
+      $('#loading').remove();
+      data = rawData.sort((a, b) => b.x - a.x);
+      updateTable();
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+      $('#loading').text('Error! ' + errorThrown);
+    },
+    complete: function() {
+      clearTimeout(loadingMessageUpdate);
+    },
+    timeout: 0,
+  });
+  $('input.minute').change(function(event) {
+    if (event.target.value > 99) {
+      event.target.value = 99;
+    }
+    if (event.target.value < 0 || !event.target.value) {
+      event.target.value = 0;
+    }
+  });
+  $('input.second').change(function(event) {
+    if (event.target.value > 59) {
+      event.target.value = 59;
+    }
+    if (event.target.value < 0 || !event.target.value) {
+      event.target.value = 0;
+    }
+    if (event.target.value < 10) {
+      event.target.value = '0' + event.target.value;
+    }
+  });
+});
