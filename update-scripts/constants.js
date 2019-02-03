9e7f8dd7674b655d21e4c844fcfd618cb2717417
Anton Kalmanovich <grumd0@gmail.com>
2019-02-03 15:40:41 +0200

    Script update, add adjust by number of people in a PP range

--- update-scripts/constants.js
+++ update-scripts/constants.js
@@ -4,4 +4,7 @@ module.exports = {
   mapInfoCacheFileName: './temp/map-cache.json',
   resultArrayJson: './temp/result-array.json',
   topMappersResultJson: './temp/data-mappers.json',
+  ppBlocksJson: './temp/pp-blocks.json',
+  ppBlockSize: 5,
+  ppBlockMapCount: 10,
 };
