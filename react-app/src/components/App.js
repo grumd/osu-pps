fca6c4dba37dfe3216ea30e37c132682d4024183
Anton Kalmanovich <grumd0@gmail.com>
2019-05-21 23:48:55 +0300

    small routing fix

--- react-app/src/components/App.js
+++ react-app/src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Redirect } from 'react-router-dom';
+import { Route, Redirect, Switch } from 'react-router-dom';
 
 import './App.scss';
 
@@ -11,26 +11,35 @@ class App extends Component {
   render() {
     return (
       <div className="container">
-        <Route exact path="/" render={() => <Redirect to="/osu/maps" />} />
-        <Route
-          path="/:mode(osu|taiko|mania|fruits)"
-          render={({ match, location }) => (
-            <React.Fragment>
-              <TopBar match={match} location={location} />
-              <Route path={`${match.path}/maps`} component={Table} />
-              <Route path={`${match.path}/mappers/:sort(total|by-age)`} component={TopMappers} />
-              <Route
-                path={`${match.path}/farmers`}
-                component={() => <div>under construction...</div>}
-              />
-              <Route
-                exact
-                path={`${match.path}/mappers`}
-                render={() => <Redirect to={`${match.url}/mappers/by-age`} />}
-              />
-            </React.Fragment>
-          )}
-        />
+        <Switch>
+          <Route exact path="/" render={() => <Redirect to="/osu/maps" />} />
+          <Route
+            path="/:mode(osu|taiko|mania|fruits)"
+            render={({ match, location }) => (
+              <React.Fragment>
+                <TopBar match={match} location={location} />
+                <Switch>
+                  <Route path={`${match.path}/maps`} component={Table} />
+                  <Route
+                    path={`${match.path}/mappers/:sort(total|by-age)`}
+                    component={TopMappers}
+                  />
+                  <Route
+                    path={`${match.path}/farmers`}
+                    component={() => <div>under construction...</div>}
+                  />
+                  <Route
+                    exact
+                    path={`${match.path}/mappers`}
+                    render={() => <Redirect to={`${match.url}/mappers/by-age`} />}
+                  />
+                  <Route render={() => <Redirect to={`${match.url}/maps`} />} />
+                </Switch>
+              </React.Fragment>
+            )}
+          />
+          <Route render={() => <Redirect to="/osu/maps" />} />
+        </Switch>
       </div>
     );
   }
