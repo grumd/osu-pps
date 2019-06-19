503286b6a3f77ce2ba2630da32bad7c37badb2f4
Anton Kalmanovich <grumd0@gmail.com>
2019-06-19 16:36:54 +0300

    Updated filtering condition and increased timeout

--- update-scripts/axios.js
+++ update-scripts/axios.js
@@ -1,14 +1,16 @@
 const axios = require('axios');
 
 const config = {
-  timeout: 10000,
+  timeout: 30000,
 };
 
 // const instance = axios.create(config);
 
 const axiosGet = (url, options = {}) => {
   const abort = axios.CancelToken.source();
-  const id = setTimeout(() => abort.cancel(`Timeout of ${config.timeout}ms.`), config.timeout);
+  const id = setTimeout(() => {
+    abort.cancel(`Timeout of ${config.timeout}ms.`);
+  }, config.timeout);
   return axios.get(url, { cancelToken: abort.token, ...options }).then(response => {
     clearTimeout(id);
     return response;
