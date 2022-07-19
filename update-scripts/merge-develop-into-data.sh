eaf80def5a48ca1d902b99e67177d40347b339c7
Anton Kalmanovich <grumd0@gmail.com>
2022-07-20 05:40:42 +0700

    Syntax fix in the sh script

--- update-scripts/merge-develop-into-data.sh
+++ update-scripts/merge-develop-into-data.sh
@@ -8,23 +8,22 @@ merge_from=develop
 echo
 if [[ $(git rev-parse --abbrev-ref HEAD) = $merge_into ]]
 then
-    echo "Currently on branch $merge_into"
-    if [[ $(git status | grep 'nothing to commit, working tree clean') ]]
-    then
-	  git checkout $merge_from
-	  git pull
-	  git checkout $merge_into
-	  git reset HEAD~1
-	  git stash -u
-	  git merge $merge_from
-	  git stash pop
-	  git add .
-	  git commit -m "Data update from"
-	  echo "Merged $merge_from to $merge_into successfully."
-    else
-      echo "ERROR: There are uncommited files in $merge_into branch!"
-    fi
-  fi
+	echo "Currently on branch $merge_into"
+	if [[ $(git status | grep 'nothing to commit, working tree clean') ]]
+	then
+		git checkout $merge_from
+		git pull
+		git checkout $merge_into
+		git reset HEAD~1
+		git stash -u
+		git merge $merge_from
+		git stash pop
+		git add .
+		git commit -m "Data update from"
+		echo "Merged $merge_from to $merge_into successfully."
+	else
+		echo "ERROR: There are uncommited files in $merge_into branch!"
+	fi
 else
-    echo "ERROR: Not on branch $merge_into - current branch is $(git rev-parse --abbrev-ref HEAD)"
+	echo "ERROR: Not on branch $merge_into - current branch is $(git rev-parse --abbrev-ref HEAD)"
 fi
