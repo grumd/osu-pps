d97d0968e49d305582d00fc7345e0b48f8bff027
Anton Kalmanovich <grumd0@gmail.com>
2022-10-30 15:22:52 +0700

    Updates

--- assets/Text.5b0ebb29.js
+++
@@ -1,1 +0,0 @@
-import{s as t,c as e}from"./index.8d92f515.js";const o=t("span",{variants:{nowrap:{true:{whiteSpace:"nowrap"}},bold:{true:{fontWeight:"bold"}},faded:{true:{color:e.textInactive}},color:{red:{color:e.textRed},green:{color:e.textGreen},blue:{color:e.textBlue}}}});o.displayName="Text";export{o as T};
